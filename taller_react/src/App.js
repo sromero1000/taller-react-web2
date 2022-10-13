e6c3b104ec1249754a51362343ae8ce4ade87d4e
= <=>
2022-10-13 11:27:47 -0500

    taller

---
+++ taller_react/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import PeticionApi from './components/PeticionAPI';
+
+function App() {
+  return (
+    <div>
+      <PeticionApi/>
+      
+    </div>
+  );
+}
+
+export default App;
